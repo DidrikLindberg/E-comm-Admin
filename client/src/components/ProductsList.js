f7d2d80ed113244a7733b1f47def98e1d2a33188
GitDidrik <lindberg.didrik@gmail.com>
2023-07-06 07:55:33 -0700

    example images

--- client/src/components/ProductsList.js
+++ client/src/components/ProductsList.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_PRODUCTS } from '../utils/queries';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+import Authservice from '../utils/auth';
 
 const ProductsList = () => {
+  // const userRole = Authservice.getUser.userRole();
   const [products, setProducts] = useState([]);
   const { loading, error, data } = useQuery(GET_PRODUCTS);
 
@@ -23,7 +28,12 @@ const ProductsList = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {products.map((product) => (
-        <div key={product.id} className="bg-white p-4 shadow-md rounded">
+        <div key={product.id} className="bg-white p-4 shadow-md rounded relative">
+          (
+            <button className="absolute top-2 right-2 bg-red-500 text-white rounded-full p-2">
+              <FontAwesomeIcon icon={faTrash} />
+            </button>
+          )}
           <h3 className="text-lg font-bold">{product.name}</h3>
           <p className="text-gray-600">{product.description}</p>
           <p className="text-gray-700 font-bold">Price: ${product.price}</p>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import productimg from '../assets/images/PartyTentBlack.png';
+import BlackCanopy from '../assets/images/BlackCanopy1.jpg';
+import WhitePartyTent from '../assets/images/WhitePartyTent.jpg';
+import BannerImg from '../assets/images/Banner.jpg';
 
 const Home = () => {
   return (
@@ -8,12 +12,12 @@ const Home = () => {
         <div className="container mx-auto py-16">
           <div className="flex flex-col lg:flex-row items-center justify-between">
             <div className="lg:w-1/2">
-              <h1 className="text-4xl lg:text-6xl font-bold mb-4">Welcome to Our New Ecommerce Website</h1>
-              <p className="text-lg text-gray-600 mb-8">Shop our collection of shirts, shoes, hats, music, and shorts.</p>
+              <h1 className="text-4xl lg:text-6xl font-bold mb-4">Prepare For Your Next Fesival with Us!</h1>
+              <p className="text-lg text-gray-600 mb-8">Shop our collection of Party Tents, Canopies, and other camping gear you might need.</p>
               <Link to="/shopping" className="btn btn-primary">Shop Now</Link>
             </div>
             <div className="lg:w-1/2">
-              <img src="/imgs/BigLandingImage.jpg" alt="Banner Image" className="w-full rounded-lg shadow-lg" />
+              <img src= {BannerImg} alt="Banner" className="w-full rounded-lg shadow-lg" />
             </div>
           </div>
         </div>
@@ -23,27 +27,45 @@ const Home = () => {
         <div className="container mx-auto">
           <h2 className="text-3xl lg:text-4xl font-bold mb-8">Featured Products</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div className="card">
-              <img src="/imgs/t-shirt.jpg" className="card-img-top img-fluid" alt="Product Image" />
-              <div className="card-body">
-                <h5 className="card-title">Plain T-Shirt</h5>
-                <p className="card-text">Price: $15.00</p>
-                <a href="#" className="btn btn-primary">Add to Cart</a>
+            <div className="card w-64 h-64">
+              <div className="relative w-full h-full">
+                <img
+                  src={BlackCanopy}
+                  alt="Product"
+                  className="absolute inset-0 w-full h-full object-cover"
+                />
               </div>
-            </div>
-            <div className="card">
-              <img src="/imgs/running shoe.jpg" className="card-img-top img-fluid" alt="Product Image" />
               <div className="card-body">
                 <h5 className="card-title">Running Shoes</h5>
                 <p className="card-text">Price: $75.00</p>
                 <a href="#" className="btn btn-primary">Add to Cart</a>
               </div>
             </div>
-            <div className="card">
-              <img src="/imgs/snapback.jpg" className="card-img-top img-fluid" alt="Product Image" />
+           <div className="card w-64 h-64">
+              <div className="relative w-full h-full">
+                <img
+                  src={productimg}
+                  alt="Product"
+                  className="absolute inset-0 w-full h-full object-cover"
+                />
+              </div>
               <div className="card-body">
-                <h5 className="card-title">Snapback Hat</h5>
-                <p className="card-text">Price: $20.00</p>
+                <h5 className="card-title">Running Shoes</h5>
+                <p className="card-text">Price: $75.00</p>
+                <a href="#" className="btn btn-primary">Add to Cart</a>
+              </div>
+            </div>
+            <div className="card w-64 h-64">
+              <div className="relative w-full h-full">
+                <img
+                  src={WhitePartyTent}
+                  alt="Product"
+                  className="absolute inset-0 w-full h-full object-cover"
+                />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Running Shoes</h5>
+                <p className="card-text">Price: $75.00</p>
                 <a href="#" className="btn btn-primary">Add to Cart</a>
               </div>
             </div>
